a9db82bf689b777529a4df5a52271d53cb67f693
PBojana <bo_sale@hotmail.com>
2017-06-12 15:09:53 +0200

    FIrst commit

---
+++ src/main/java/GItHubbet356Kraaen/GItHubbet356Kraaen/App.java
@@ -0,0 +1,13 @@
+package GItHubbet356Kraaen.GItHubbet356Kraaen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
